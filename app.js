95f222cd3f8016ca82ccde4c95f7cb98eb8b4296
barhan44 <gunyashov44@gmail.com>
2020-06-14 18:59:45 +0300

    fix: warning

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const positionRoutes = require('./routes/position.routes');
 
 const app = express();
 mongoose
-  .connect(configs.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(configs.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB connected!'))
   .catch((err) => console.error(err));
 
