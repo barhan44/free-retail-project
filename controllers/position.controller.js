2ef1dc0bab53588bf3cf9f86b9b93813c985159e
barhan44 <gunyashov44@gmail.com>
2020-06-15 12:13:20 +0300

    Squashed commit of the following:
    commit 7f505218a581561850d43f01856ce376abfdf7ed
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:12:37 2020 +0300
    
        Add: update position
    
    commit ad7a4faf91850c8cba315b4cba57f83ac38674f2
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:09:27 2020 +0300
    
        Add: remove position
    
    commit 5a9cb79fd2f7d3fe6dba7fb2dc1984fbe06303a9
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:07:22 2020 +0300
    
        Add: create position
    
    commit 34e8a98acd1d4e4c07b4ad21df328b206c250c5d
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:03:46 2020 +0300
    
        Add: find category by id

--- controllers/position.controller.js
+++ controllers/position.controller.js
@@ -1,7 +1,53 @@
-module.exports.getByCategoryID = function (req, res) {};
+const Position = require('../models/Position.model');
+const errorHandler = require('../utils/errorHandler');
 
-module.exports.create = function (req, res) {};
+module.exports.getByCategoryID = async function (req, res) {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryID,
+      user: req.user.id,
+    });
+    res.status(200).json(positions);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-module.exports.remove = function (req, res) {};
+module.exports.create = async function (req, res) {
+  const { name, cost, category } = req.body;
+  try {
+    const position = await new Position({
+      name,
+      cost,
+      category,
+      user: req.user.id,
+    }).save();
+    res.status(201).json(position);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-module.exports.update = function (req, res) {};
+module.exports.remove = async function (req, res) {
+  try {
+    await Position.remove({
+      _id: req.params.id,
+    });
+    res.status(200).json({ message: 'The position has been deleted' });
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
+module.exports.update = async function (req, res) {
+  try {
+    const position = await Position.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(position);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
