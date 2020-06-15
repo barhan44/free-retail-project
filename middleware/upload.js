f5dee60c94794b9a754dd941f27a89ccf6fa9214
barhan44 <gunyashov44@gmail.com>
2020-06-15 12:50:15 +0300

    Squashed commit of the following:
    commit 4144c56f1ca669139561caa3b6d5aa7377474d44
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:49:21 2020 +0300
    
        Add: update category
    
    commit c797f0165e71da7f5d07d74e30c9044ef9836532
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:46:28 2020 +0300
    
        Add: create category
    
    commit 24dd6ab48af98d54fa39eab9654cc798ab4fe70a
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:34:19 2020 +0300
    
        Add: upload configuration
    
    commit c348ac909ec5a1a2ee8d9045832dc676f2559692
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:22:43 2020 +0300
    
        Add: multer + moment packages
    
    commit 2267494d56d8c02eaf971bdd6443a365ab52c96d
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:21:00 2020 +0300
    
        Add: delete category
    
    commit 1d6bf13a8ab63c1f7d0754875f932284f208fbe6
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:17:48 2020 +0300
    
        Add: get category by id
    
    commit d94256395992ee6671df2803f1e5c87c8d815911
    Author: barhan44 <gunyashov44@gmail.com>
    Date:   Mon Jun 15 12:16:37 2020 +0300
    
        Add: get all categories

---
+++ middleware/upload.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    const date = moment().format('DDMMYYYY-HHmmss_SSS');
+    cb(null, `${date}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const supportedFileTypes = ['image/png', 'image/jpeg'];
+  supportedFileTypes.includes(file.mimetype) ? cb(null, true) : cb(null, false);
+};
+
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+module.exports = multer({
+  storage,
+  fileFilter,
+  limits,
+});
