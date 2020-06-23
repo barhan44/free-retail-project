2e6fcab83a3ced19a94c6d59cff1c5b12d87f42d
barhan44 <gunyashov44@gmail.com>
2020-06-23 23:17:25 +0300

    Add: analytics

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { Chart } from 'chart.js';
+import { AnalyticsService } from '../shared/services/analytics.service';
+import { AnalyticsPage } from '../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
-  styleUrls: ['./analytics-page.component.scss']
+  styleUrls: ['./analytics-page.component.scss'],
 })
-export class AnalyticsPageComponent implements OnInit {
+export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('gain') gainRef: ElementRef;
+  @ViewChild('order') orderRef: ElementRef;
 
-  constructor() { }
+  analyticsSub: Subscription;
+  average: number;
+  pending = true;
 
-  ngOnInit(): void {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  ngAfterViewInit(): void {
+    const gainConfig: any = {
+      label: 'Выручка',
+      color: 'rgb(255, 99, 132)',
+    };
+
+    const orderConfig: any = {
+      label: 'Заказы',
+      color: 'rgb(54, 162, 235)',
+    };
+
+    this.analyticsSub = this.analyticsService
+      .getAnalytics()
+      .subscribe((data: AnalyticsPage) => {
+        this.average = data.average;
+        gainConfig.labels = data.chart.map((item) => item.label);
+        gainConfig.data = data.chart.map((item) => item.gain);
+
+        orderConfig.labels = data.chart.map((item) => item.label);
+        orderConfig.data = data.chart.map((item) => item.order);
+
+        const gainContext = this.gainRef.nativeElement.getContext('2d');
+        const orderContext = this.orderRef.nativeElement.getContext('2d');
+        gainContext.canvas.height = '300px';
+        orderContext.canvas.height = '300px';
+
+        new Chart(gainContext, createChartConfig(gainConfig));
+        new Chart(orderContext, createChartConfig(orderConfig));
+
+        this.pending = false;
+      });
   }
 
+  ngOnDestroy(): void {
+    if (this.analyticsSub) {
+      this.analyticsSub.unsubscribe();
+    }
+  }
+}
+
+function createChartConfig({ labels, data, label, color }) {
+  return {
+    type: 'line',
+    options: {
+      responsive: true,
+    },
+    data: {
+      labels,
+      datasets: [
+        { label, data, borderColor: color, steppedLine: false, fill: false },
+      ],
+    },
+  };
 }
--- controllers/analytics.controller.js
+++ controllers/analytics.controller.js
@@ -1,3 +1,98 @@
-module.exports.overview = function (req, res) {};
+const moment = require('moment');
 
-module.exports.analytics = function (req, res) {};
+const Order = require('../models/Order.model');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.overview = async function (req, res) {
+  try {
+    const allOrders = await Order.find({ user: req.user.id }).sort({ date: 1 });
+    const ordersMap = getOrdersMap(allOrders);
+    const prevDayOrders =
+      ordersMap[moment().add(-1, 'd').format('DD.MM.YYYY')] || [];
+
+    const prevDayOrdersNumber = prevDayOrders.length;
+    const totalOrders = allOrders.length;
+    const daysNumber = Object.keys(ordersMap).length;
+    const totalOrdersPerDay = (totalOrders / daysNumber).toFixed(0);
+    const ordersPercent = (
+      (prevDayOrdersNumber / totalOrdersPerDay - 1) *
+      100
+    ).toFixed(2);
+    const totalGain = calcPrice(allOrders);
+    const dailyGain = totalGain / daysNumber;
+    const prevDayGain = calcPrice(prevDayOrders);
+    const gainPercent = ((prevDayGain / dailyGain - 1) * 100).toFixed(2);
+    const compareGain = (prevDayGain - dailyGain).toFixed(2);
+    const compareOrders = (prevDayOrdersNumber - totalOrdersPerDay).toFixed(2);
+
+    res.status(200).json({
+      gain: {
+        percent: Math.abs(+gainPercent),
+        compare: Math.abs(+compareGain),
+        prevDay: +prevDayGain,
+        isHigher: gainPercent > 0,
+      },
+      orders: {
+        percent: Math.abs(+ordersPercent),
+        compare: Math.abs(+compareOrders),
+        prevDay: +prevDayOrdersNumber,
+        isHigher: ordersPercent > 0,
+      },
+    });
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
+module.exports.analytics = async function (req, res) {
+  try {
+    const allOrders = await Order.find({ user: req.user.id }).sort({ date: 1 });
+    const ordersMap = getOrdersMap(allOrders);
+
+    const average = +(
+      calcPrice(allOrders) / Object.keys(ordersMap).length
+    ).toFixed(2);
+
+    const chart = Object.keys(ordersMap).map((label) => {
+      const gain = calcPrice(ordersMap[label]);
+      const order = ordersMap[label].length;
+      return {
+        label,
+        order,
+        gain,
+      };
+    });
+
+    res.status(200).json({
+      average,
+      chart,
+    });
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
+function getOrdersMap(orders = []) {
+  const daysOrders = {};
+  orders.forEach((order) => {
+    const date = moment(order.date).format('DD.MM.YYYY');
+    if (date === moment().format('DD.MM.YYYY')) {
+      return;
+    }
+    if (!daysOrders[date]) {
+      daysOrders[date] = [];
+    }
+
+    daysOrders[date].push(order);
+  });
+  return daysOrders;
+}
+
+function calcPrice(orders = []) {
+  return orders.reduce((total, order) => {
+    const orderPrice = order.list.reduce((orderTotal, item) => {
+      return (orderTotal += item.cost * item.quantity);
+    }, 0);
+    return (total += orderPrice);
+  }, 0);
+}
